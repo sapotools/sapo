0103ead1c76d5520b6deb5f52bdcc218956263f4
Alberto Casagrande <acasagrande@units.it>
2021-11-01 05:22:31 +0100

    Replaced VarsGenerator class by get_symbol_lst function.

--- include/VarsGenerator.h
+++ include/VarsGenerator.h
@@ -1,57 +1,25 @@
 /**
  * @file VarsGenerator.h
- * Automatically generate variables for paralleltope generator functions.
- * For high dimensions declaring manually the variables can be tedious...
+ * Automatically generate variables.
  *
- * @author Tommaso Dreossi <tommasodreossi@berkeley.edu>
+ * @author Alberto Casagrande <acasagrande@units.it>
  * @version 0.1
  */
 
 #ifndef VARSGENERATOR_H_
 #define VARSGENERATOR_H_
 
-#include "Common.h"
-#include "LinearSystem.h"
+#include<string>
 
-class VarsGenerator
+#include <ginac/ginac.h>
+
+GiNaC::lst get_symbol_lst(const std::string &basename,
+                          const unsigned int number_of_symbols);
+
+inline GiNaC::lst get_symbol_lst(const char *basename,
+                                 const unsigned int number_of_symbols)
 {
-
-private:
-  unsigned int dim;
-
-  GiNaC::lst qs;              // base vertex
-  GiNaC::lst as;              // alphas (free variables)
-  GiNaC::lst bs;              // betas (lenghts)
-  GiNaC::lst ls;              // directions (lambdas)
-  std::vector<GiNaC::lst> us; // genertor versors
-
-public:
-  VarsGenerator(const unsigned int dim);
-
-  const GiNaC::lst &getBaseVertex() const
-  {
-    return this->qs;
-  };
-  const GiNaC::lst &getFreeVars() const
-  {
-    return this->as;
-  };
-  const GiNaC::lst &getLenghts() const
-  {
-    return this->bs;
-  };
-  const GiNaC::lst &getDirections() const
-  {
-    return this->ls;
-  };
-  const std::vector<GiNaC::lst> &getVersors() const
-  {
-    return this->us;
-  };
-
-  LinearSystem genBox(const std::vector<double> &b) const;
-
-  virtual ~VarsGenerator();
-};
+  return get_symbol_lst(std::string(basename), number_of_symbols);
+}
 
 #endif /* VARSGENERATOR_H_*/
--- src/VarsGenerator.cpp
+++ src/VarsGenerator.cpp
@@ -1,9 +1,8 @@
 /**
  * @file VarsGenerator.cpp
- * Automatically generate variables for paralleltope generator functions.
- * For high dimensions declaring manually the variables can be tedious...
+ * Automatically generate variables.
  *
- * @author Tommaso Dreossi <tommasodreossi@berkeley.edu>
+ * @author Alberto Casagrande <acasagrande@units.it>
  * @version 0.1
  */
 
@@ -25,58 +24,4 @@ GiNaC::lst get_symbol_lst(const std::string &basename,
   }
 
   return symbol_lst;
-}
-
-inline GiNaC::lst get_symbol_lst(const char *basename,
-                                 const unsigned int number_of_symbols)
-{
-  return get_symbol_lst(std::string(basename), number_of_symbols);
-}
-
-/**
- * Constructor that instantiates the variable generator
- *
- * @param[in] dim dimension of the model/parallelotope
- */
-VarsGenerator::VarsGenerator(const unsigned int dim):
-    dim(dim), qs(get_symbol_lst("q", dim)), as(get_symbol_lst("a", dim)),
-    bs(get_symbol_lst("b", dim)), ls(get_symbol_lst("l", dim))
-{
-  using namespace GiNaC;
-
-  lst symbol_lst;
-
-  for (unsigned int i = 0; i < dim; ++i) {
-    std::ostringstream oss;
-    oss << "u" << i << "_";
-
-    this->us.push_back(get_symbol_lst(oss.str(), dim));
-  }
-}
-
-/**
- * Generate a box out of the variables
- *
- * @param[in] b box offsets
- * @returns generatred box
- */
-LinearSystem VarsGenerator::genBox(const std::vector<double> &b) const
-{
-  using namespace std;
-
-  unsigned int n = b.size() / 2;
-  vector<double> Ai(n, 0);
-  vector<vector<double>> A(2 * n, Ai);
-
-  for (unsigned int i = 0; i < n; i++) {
-    A[2 * i][i] = 1;
-    A[2 * i + 1][i] = -1;
-  }
-
-  return LinearSystem(A, b);
-}
-
-VarsGenerator::~VarsGenerator()
-{
-  // TODO Auto-generated destructor stub
 }
