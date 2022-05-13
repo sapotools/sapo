3d884f1705b3e1dfce706e6b6410964857adaad8
Alberto Casagrande <acasagrande@units.it>
2022-05-13 15:33:21 +0200

    Bug fixed `Parallelotope` class

--- libSapo/src/Parallelotope.cpp
+++ libSapo/src/Parallelotope.cpp
@@ -65,154 +65,27 @@ Parallelotope::Parallelotope(
 }
 
 /**
- * Determine the equation of the hyperplane passing through some linearly
- * independent points, i.e., res[0]*x_0 + .. + res[n-1]*x_{n-1} + res[n] = 0
- *
- * @param[in] pts interpolation points
- * @returns interpolating function coefficients
- */
-std::vector<double>
-hyperplane_through_points(const std::list<std::vector<double>> &pts)
-{
-  using namespace std;
-  using namespace LinearAlgebra;
-
-  if (pts.size() == 0 || pts.size() != pts.front().size()) {
-    std::domain_error("hyperplane_through_points: pts must contain "
-                      "n non-linearly dependent n-dimensional points");
-  }
-
-  // build the matrix A=[pts[1]-pts[0],...,pts[n-2]-pts[0]]^T
-  Sparse::Matrix<double> A;
-  for (auto pts_it = ++std::begin(pts); pts_it != std::end(pts); ++pts_it) {
-    A.add_row(pts.front() - *pts_it);
-  }
-
-  // factorize the A
-  Sparse::LUP_Factorization<double> fact(A);
-
-  // A is underdetermined because it is a (n-1)x n matrix
-  // find the underdetermined dimension udim
-  unsigned int udim = 0;
-  const Sparse::Matrix<double> &U = fact.U();
-  while (udim < U.num_of_rows() && U[udim][udim] != 0) {
-    ++udim;
-  }
-
-  // add to A the versor on udim
-  Sparse::Matrix<double>::RowType row;
-  row[udim] = 1;
-  A.add_row(row);
-
-  // factorize the new matrix
-  fact = LinearAlgebra::Sparse::LUP_Factorization<double>(A);
-
-  std::vector<double> lambda;
-  try {
-    // try to solve the new factorization: if it is not possible
-    // the points did not define an n-dimensional hyperplane.
-    lambda = fact.solve(std::vector<double>(A.num_of_rows(), 0));
-
-  } catch (std::domain_error &) {
-    std::domain_error("hyperplane_through_points: the points "
-                      "are linearly dependent.");
-  }
-
-  // add the offset to the result
-  const double offset = -(lambda * pts.front());
-  lambda.push_back(offset);
-
-  return lambda;
-}
-
-// TODO: Fix the method below
-/**
  * Build a polytope representing the parallelotope.
  *
  * @returns a polytope representing the parallelotope
  */
 Parallelotope::operator Polytope() const
 {
-  using namespace std;
   using namespace LinearAlgebra;
 
-  const unsigned int dim = this->dim();
+  std::vector<Vector<double>> A(2*dim());
+  Vector<double> b(2*dim());
+  for (unsigned int i=0; i<dim(); ++i) {
+    const double base_bound = _generators[i]*_base_vertex;
+    const double vertex_bound = base_bound + _lengths[i];
 
-  std::vector<std::vector<double>> hps; // hyperplane equations
+    A[i] = Vector<double>(_generators[i]);
+    A[i+dim()] = -_generators[i];
+    b[i] = std::max(base_bound, vertex_bound);
+    b[i+dim()] = -std::min(base_bound, vertex_bound);
+  } 
 
-  // first set of hyperplanes
-  for (unsigned int i = 0; i < dim; i++) {
-
-    std::list<std::vector<double>> pts;
-    pts.push_back(_base_vertex); // add base vertex
-
-    for (unsigned int j = 0; j < dim; j++) { // for all the generators u
-      if (i != j) {
-        if (_lengths[j] != 0) {
-          pts.push_back(pts.front() + _lengths[j] * this->_generators[j]);
-        } else {
-          // these are fake points to define one hypeplane in
-          // degenerous parallelotypes, for instance, when one
-          // of the variables admits one single value.
-          pts.push_back(pts.front() + this->_generators[j]);
-        }
-      }
-    }
-    hps.push_back(hyperplane_through_points(pts));
-  }
-
-  // second set of hyperplanes
-  for (unsigned int i = 0; i < dim; i++) {
-
-    std::list<std::vector<double>> pts;
-
-    // add base vertex
-    pts.push_back(_base_vertex + _lengths[i] * _generators[i]);
-
-    for (unsigned int j = 0; j < dim; j++) {
-      // for all the generators u
-      if (i != j) {
-        if (_lengths[j] != 0) {
-          pts.push_back(pts.front() + _lengths[j] * this->_generators[j]);
-        } else {
-          // these are fake points to define one hypeplane in
-          // degenerous parallelotypes, for instance, when one
-          // of the variables admits one single value.
-          pts.push_back(pts.front() + this->_generators[j]);
-        }
-      }
-    }
-
-    hps.push_back(hyperplane_through_points(pts));
-  }
-
-  std::vector<double> d(dim * 2, 0);
-  std::vector<std::vector<double>> Lambda(d.size(),
-                                          std::vector<double>(dim, 0));
-
-  for (unsigned int i = 0; i < dim; i++) {
-
-    const std::vector<double> &hps_i = hps[i];
-    const std::vector<double> &hps_i_plus = hps[i + dim];
-
-    std::vector<double> &Lambda_i = Lambda[i];
-    std::vector<double> &Lambda_i_plus = Lambda[i + dim];
-
-    // find hyperplane with smaller direction
-    double b1 = -hps_i[hps[i].size() - 1];
-    double b2 = -hps_i_plus[hps[i].size() - 1];
-
-    double sign = (b1 > b2 ? 1 : -1);
-
-    d[i] = sign * b1;
-    d[i + dim] = -sign * b2;
-    for (unsigned int j = 0; j < dim; j++) {
-      Lambda_i[j] = sign * hps_i[j];
-      Lambda_i_plus[j] = -sign * hps_i_plus[j];
-    }
-  }
-
-  return Polytope(Lambda, d);
+  return Polytope(A, b);
 }
 
 void swap(Parallelotope &A, Parallelotope &B)
