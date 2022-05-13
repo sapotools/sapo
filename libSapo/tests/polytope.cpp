42e5e86f2688484d59761eff8a5aeab448f6cb34
Alberto Casagrande <acasagrande@units.it>
2022-05-13 09:35:21 +0200

    Improved polytope tests

--- libSapo/tests/polytope.cpp
+++ libSapo/tests/polytope.cpp
@@ -69,9 +69,25 @@ BOOST_AUTO_TEST_CASE(test_contains_polytope)
         {0,0,-1}
     };
 
+    Matrix<double> B = {
+        {1,1,0},
+        {1,-1,0},
+        {0,1,1,0},
+        {0,-1,1},
+        {1,0,1},
+        {1,0,-1},
+        {-1,0,-1},
+        {-1,0,1},
+        {0,-1,-1},
+        {0,1,-1},
+        {-1,-1,0},
+        {-1,1,0},
+    };
+
     Polytope p1(A,{1,2,3,3,2,1}), p2(A,{1,2,3,0,0,0}), 
              p3(A,{3,4,5,0,0,0}), p4(A,{3,4,5,-2,-3,-4}), 
-             p5(A,{1,2,3,-3,2,1});
+             p5(A,{1,2,3,-3,2,1}), 
+             p6(B,{1,1,3,3,0,2,4,2,1,1,3,3});
 
     BOOST_CHECK(p5.is_empty());
 
@@ -80,6 +96,7 @@ BOOST_AUTO_TEST_CASE(test_contains_polytope)
     BOOST_CHECK(p2.contains(p5));
     BOOST_CHECK(p3.contains(p2));
     BOOST_CHECK(p5.contains(p5));
+    BOOST_CHECK(p1.contains(p6));
     BOOST_CHECK(!p3.contains(p1));
     BOOST_CHECK(!p1.contains(p3));
     BOOST_CHECK(!p2.contains(p3));
@@ -88,6 +105,7 @@ BOOST_AUTO_TEST_CASE(test_contains_polytope)
     BOOST_CHECK(!p5.contains(p1));
     BOOST_CHECK(!p2.contains(p1));
     BOOST_CHECK(!p2.contains(p3));
+    BOOST_CHECK(!p6.contains(p1));
 }
 
 BOOST_AUTO_TEST_CASE(test_intersect_polytope)
@@ -104,9 +122,25 @@ BOOST_AUTO_TEST_CASE(test_intersect_polytope)
         {0,0,-1}
     };
 
+    Matrix<double> B = {
+        {1,1,0},
+        {1,-1,0},
+        {0,1,1,0},
+        {0,-1,1},
+        {1,0,1},
+        {1,0,-1},
+        {-1,0,-1},
+        {-1,0,1},
+        {0,-1,-1},
+        {0,1,-1},
+        {-1,-1,0},
+        {-1,1,0},
+    };
+
     Polytope p1(A,{1,2,3,3,2,1}), p2(A,{1,2,3,0,0,0}), 
              p3(A,{3,4,5,0,0,0}), p4(A,{3,4,5,-2,-3,-4}), 
-             p5(A,{1,2,3,-3,2,1});
+             p5(A,{1,2,3,-3,2,1}), 
+             p6(B,{1,1,3,3,0,2,4,2,1,1,3,3});
 
     Polytope pa = intersect(p1, p2),
              pb = intersect(p2, p1);
@@ -151,6 +185,13 @@ BOOST_AUTO_TEST_CASE(test_intersect_polytope)
     BOOST_CHECK(p5.is_empty());
     BOOST_CHECK(pa.is_empty());
     BOOST_CHECK(!p1.is_empty());
+
+    pa = intersect(p1, p6);
+    pb = intersect(p6, p1);
+
+    BOOST_CHECK(pa == pb);
+    BOOST_CHECK(pa == p6);
+    BOOST_CHECK(pa != p1);
 }
 
 
@@ -168,9 +209,24 @@ BOOST_AUTO_TEST_CASE(test_intersect_with_polytope)
         {0,0,-1}
     };
 
+    Matrix<double> B = {
+        {1,1,0},
+        {1,-1,0},
+        {0,1,1,0},
+        {0,-1,1},
+        {1,0,1},
+        {1,0,-1},
+        {-1,0,-1},
+        {-1,0,1},
+        {0,-1,-1},
+        {0,1,-1},
+        {-1,-1,0},
+        {-1,1,0},
+    };
     Polytope p1(A,{1,2,3,3,2,1}), p2(A,{1,2,3,0,0,0}), 
              p3(A,{3,4,5,0,0,0}), p4(A,{3,4,5,-2,-3,-4}), 
-             p5(A,{1,2,3,-3,2,1});
+             p5(A,{1,2,3,-3,2,1}),
+             p6(B,{1,1,3,3,0,2,4,2,1,1,3,3});
 
     Polytope pa = p1, pb = p2;
     
@@ -205,5 +261,12 @@ BOOST_AUTO_TEST_CASE(test_intersect_with_polytope)
     pa.intersect_with(p5);
     pb.intersect_with(p1);
     BOOST_CHECK(intersect(p1, p5) == pa);
+    BOOST_CHECK(pa == pb);
+
+    pa = p1;
+    pb = p6;
+    pa.intersect_with(p6);
+    pb.intersect_with(p1);
+    BOOST_CHECK(intersect(p1, p6) == pa);
     BOOST_CHECK(pa == pb);
 }
