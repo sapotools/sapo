05ecad8ead9cf5614afc5509ab7acd557512f42b
Alberto Casagrande <acasagrande@units.it>
2022-05-30 10:07:43 +0200

    Added tests for sets union intersections

--- libSapo/tests/sets_unions.cpp
+++ libSapo/tests/sets_unions.cpp
@@ -545,3 +545,151 @@ BOOST_AUTO_TEST_CASE(test_update_sets_union)
 
     BOOST_REQUIRE_THROW(res.update(Pu5), std::domain_error);
 }
+
+
+BOOST_AUTO_TEST_CASE(test_intersect_sets_union)
+{
+    using namespace LinearAlgebra;
+    using namespace LinearAlgebra::Dense;
+
+    Matrix<double> A = {
+        {1,0,0},
+        {0,1,0},
+        {0,0,1},
+        {-1,0,0},
+        {0,-1,0},
+        {0,0,-1}
+    };
+
+    Matrix<double> B = {
+        {1,1,0},
+        {1,-1,0},
+        {0,1,1,0},
+        {0,-1,1},
+        {1,0,1},
+        {1,0,-1},
+        {-1,0,-1},
+        {-1,0,1},
+        {0,-1,-1},
+        {0,1,-1},
+        {-1,-1,0},
+        {-1,1,0},
+    };
+
+    Matrix<double> C = {
+        {1,0},
+        {0,1},
+        {-1,0},
+        {0,-1}
+    };
+
+    Polytope p1(A,{3,4,6,3,2,1}), p2(A,{1,2,3,0,0,0}), 
+             p3(A,{4,4,5,0,0,0}), p4(A,{3,4,6,-2,-3,-4}), 
+             p5(A,{1,2,3,-3,2,1}), 
+             p6(B,{1,1,3,3,0,2,4,2,1,1,3,3}),
+             p7(C,{1,1,0,0}), p8(A,{0,1,3,3,2,1}), 
+             p9(A,{4,4,5,-1,-1,-1});
+
+    BOOST_CHECK(!p1.is_empty());
+    BOOST_CHECK(!p2.is_empty());
+    BOOST_CHECK(!p3.is_empty());
+    BOOST_CHECK(!p4.is_empty());
+    BOOST_CHECK(p5.is_empty());
+    BOOST_CHECK(!p6.is_empty());
+    BOOST_CHECK(!p7.is_empty());
+    BOOST_CHECK(!p8.is_empty());
+    BOOST_CHECK(!p9.is_empty());
+
+    BOOST_CHECK(p1.includes(p2));
+    BOOST_CHECK(p1.includes(p4));
+    BOOST_CHECK(p1.includes(p6));
+
+    BOOST_CHECK(!p2.includes(p1));
+    BOOST_CHECK(!p4.includes(p1));
+    BOOST_CHECK(!p6.includes(p1));
+
+    BOOST_CHECK(!p3.includes(p1));
+    BOOST_CHECK(!p1.includes(p3));
+
+    BOOST_CHECK(p3.includes(p2));
+    BOOST_CHECK(!p3.includes(p4));
+    BOOST_CHECK(!p3.includes(p6));
+
+    BOOST_CHECK(!p2.includes(p3));
+    BOOST_CHECK(!p4.includes(p3));
+    BOOST_CHECK(!p6.includes(p3));
+
+    SetsUnion<Polytope> Pu1({p1,p3}), Pu2({p2,p4,p6}), Pu3({p8, p9}),
+                        Pu4({intersect(p2, p8), p1}), Pu5({p7}), res;
+
+    res = intersect(Pu1, Pu1);
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu1.any_includes(*it));
+    }
+    for (auto it=std::begin(Pu1); it != std::end(Pu1); ++it) {
+        BOOST_CHECK(res.any_includes(*it));
+    }
+
+    res = intersect(Pu1, Pu2);
+
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu1.any_includes(*it)&&Pu2.any_includes(*it));
+    }
+
+    res = intersect(Pu1, Pu3);
+
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu1.any_includes(*it)&&Pu3.any_includes(*it));
+    }
+
+    res = intersect(Pu1, Pu4);
+
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu1.any_includes(*it)&&Pu4.any_includes(*it));
+    }
+
+    res = intersect(Pu2, Pu2);
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu2.any_includes(*it));
+    }
+    for (auto it=std::begin(Pu2); it != std::end(Pu2); ++it) {
+        BOOST_CHECK(res.any_includes(*it));
+    }
+
+    res = intersect(Pu2, Pu3);
+
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu2.any_includes(*it)&&Pu3.any_includes(*it));
+    }
+
+    res = intersect(Pu2, Pu4);
+
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu2.any_includes(*it)&&Pu4.any_includes(*it));
+    }
+
+    res = intersect(Pu3, Pu3);
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu3.any_includes(*it));
+    }
+    for (auto it=std::begin(Pu3); it != std::end(Pu3); ++it) {
+        BOOST_CHECK(res.any_includes(*it));
+    }
+
+    res = intersect(Pu3, Pu4);
+
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu3.any_includes(*it)&&
+                    Pu4.any_includes(*it));
+    }
+
+    res = intersect(Pu4, Pu4);
+    for (auto it=std::begin(res); it != std::end(res); ++it) {
+        BOOST_CHECK(Pu4.any_includes(*it));
+    }
+    for (auto it=std::begin(Pu4); it != std::end(Pu4); ++it) {
+        BOOST_CHECK(res.any_includes(*it));
+    }
+
+    BOOST_CHECK(intersect(Pu2, SetsUnion<Polytope>()).is_empty());
+}
