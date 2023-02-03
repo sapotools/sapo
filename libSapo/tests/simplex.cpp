9e2b3964e3afa8c809a63c20dde2ae35f37b9a2e
Alberto Casagrande <acasagrande@units.it>
2023-02-03 22:40:15 +0100

    Added more `SimplexMethodOptimizer` tests

--- libSapo/tests/simplex.cpp
+++ libSapo/tests/simplex.cpp
@@ -1,19 +1,52 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE linear_system
+#define BOOST_TEST_MODULE simplex
 
 #include <boost/test/unit_test.hpp>
 #include <boost/mpl/list.hpp>
 
+#include <gmpxx.h>
+
 #include "Simplex.h"
 #include "LinearAlgebraIO.h"
 
+typedef boost::mpl::list<double, mpq_class> test_types;
 
-BOOST_AUTO_TEST_CASE(test_simplex_optimum)
+template<typename T>
+inline T admitted_error();
+
+template<>
+inline double admitted_error<double>()
+{
+    return 1e-14;
+}
+
+template<>
+inline mpq_class admitted_error<mpq_class>()
+{
+    return 0;
+}
+
+template<typename T>
+inline bool approximate(const OptimizationResult<T>& result, const T exact)
+{
+    return (result.status()==result.OPTIMUM_AVAILABLE &&
+            abs(result.objective_value()-exact) <= admitted_error<T>());
+}
+
+template<typename T>
+inline bool is_exactly(const OptimizationResult<T>& result, const T exact)
+{
+    return (result.status()==result.OPTIMUM_AVAILABLE &&
+            result.objective_value()==exact);
+}
+
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_optimum, T, test_types)
 {
     using namespace LinearAlgebra;
     using namespace LinearAlgebra::Dense;
 
-    Matrix<double> A = {
+    Matrix<T> A = {
         {1,0,0},
         {0,1,0},
         {0,0,1},
@@ -22,9 +55,9 @@ BOOST_AUTO_TEST_CASE(test_simplex_optimum)
         {0,0,-1}
     };
 
-    Vector<double> b = {1,2,3,3,2,1};
+    Vector<T> b = {1,2,3,3,2,1};
 
-    std::vector<std::pair<std::pair<Vector<double>, OptimizationGoal>, 
+    std::vector<std::pair<std::pair<Vector<T>, OptimizationGoal>, 
                           double>> min_probs = {
         {{{1,0,0}, OptimizationGoal::MAXIMIZE}, 1},
         {{{0,1,0}, OptimizationGoal::MAXIMIZE}, 2},
@@ -57,25 +90,25 @@ BOOST_AUTO_TEST_CASE(test_simplex_optimum)
     }
 }
 
-BOOST_AUTO_TEST_CASE(test_simplex_unbounded)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_unbounded, T, test_types)
 {
     using namespace LinearAlgebra;
     using namespace LinearAlgebra::Dense;
 
-    Matrix<double> A = {
+    Matrix<T> A = {
         {0,1,0},
         {0,0,1},
         {-1,0,0},
         {0,-1,0}
     };
 
-    Vector<double> b = {2,3,3,2};
+    Vector<T> b = {2,3,3,2};
 
     SimplexMethodOptimizer optimizer;
 
-    Vector<double> obj{1,0,0};
+    Vector<T> obj{1,0,0};
     auto result = optimizer(A, b, obj, OptimizationGoal::MAXIMIZE);
-    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<double>::UNBOUNDED,
+    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<T>::UNBOUNDED,
                         "maximizing " << obj << " on " << 
                         "A x<= b where A: " << 
                         A << " and b: " << b <<
@@ -84,7 +117,7 @@ BOOST_AUTO_TEST_CASE(test_simplex_unbounded)
 
     obj = {0,0,1};
     result = optimizer(A, b, obj, OptimizationGoal::MINIMIZE);
-    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<double>::UNBOUNDED,
+    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<T>::UNBOUNDED,
                         "minimizing " << obj << " on " << 
                         "A x<= b where A: " << 
                         A << " and b: " << b <<
@@ -93,23 +126,23 @@ BOOST_AUTO_TEST_CASE(test_simplex_unbounded)
 }
 
 
-BOOST_AUTO_TEST_CASE(test_simplex_unbounded2)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_unbounded2, T, test_types)
 {
     using namespace LinearAlgebra;
     using namespace LinearAlgebra::Dense;
 
-    Matrix<double> A = {
+    Matrix<T> A = {
         {1,0},
         {0,1}
     };
 
-    Vector<double> b = {2,3};
+    Vector<T> b = {2,3};
 
     SimplexMethodOptimizer optimizer;
 
-    Vector<double> obj{1,1};
+    Vector<T> obj{1,1};
     auto result = optimizer(A, b, obj, OptimizationGoal::MAXIMIZE);
-    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<double>::OPTIMUM_AVAILABLE,
+    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<T>::OPTIMUM_AVAILABLE,
                         "maximizing " << obj << " on " << 
                         "A x<= b where A: " << 
                         A << " and b: " << b <<
@@ -117,7 +150,7 @@ BOOST_AUTO_TEST_CASE(test_simplex_unbounded2)
                         ": OPTIMUM_AVAILABLE was expected.");
 
     result = optimizer(A, b, obj, OptimizationGoal::MINIMIZE);
-    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<double>::UNBOUNDED,
+    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<T>::UNBOUNDED,
                         "minimizing " << obj << " on " << 
                         "A x<= b where A: " << 
                         A << " and b: " << b <<
@@ -125,12 +158,12 @@ BOOST_AUTO_TEST_CASE(test_simplex_unbounded2)
                         ": UNBOUNDED was expected.");
 }
 
-BOOST_AUTO_TEST_CASE(test_simplex_unfeasible)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_unfeasible, T, test_types)
 {
     using namespace LinearAlgebra;
     using namespace LinearAlgebra::Dense;
 
-    Matrix<double> A = {
+    Matrix<T> A = {
         {1,0,0},
         {0,1,0},
         {0,0,1},
@@ -139,14 +172,14 @@ BOOST_AUTO_TEST_CASE(test_simplex_unfeasible)
         {0,0,-1}
     };
 
-    Vector<double> b = {1,2,3,-3,2,1};
+    Vector<T> b = {1,2,3,-3,2,1};
 
-    Vector<double> obj{1,0,0};
+    Vector<T> obj{1,0,0};
     
     SimplexMethodOptimizer optimizer;
 
     auto result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
-    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<double>::INFEASIBLE,
+    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<T>::INFEASIBLE,
                         "maximizing " << obj << " on "
                         "A x<= b where A: " << 
                         A << " and b: " << b <<
@@ -155,10 +188,163 @@ BOOST_AUTO_TEST_CASE(test_simplex_unfeasible)
 
     obj = {0,0,1};
     result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
-    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<double>::INFEASIBLE,
+    BOOST_CHECK_MESSAGE(result.status()==OptimizationResult<T>::INFEASIBLE,
                         "minimizing " << obj << " on "
                         "A x<= b where A: " << 
                         A << " and b: " << b << 
                         " produces " << result.status() << 
                         ": INFEASIBLE was expected.");
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_3D_square, T, test_types)
+{
+    using namespace LinearAlgebra;
+    using namespace LinearAlgebra::Dense;
+
+    Matrix<T> A = {
+        {1,0,0},
+        {0,1,0},
+        {0,0,1},
+        {-1,0,0},
+        {0,-1,0},
+        {0,0,-1},
+    };
+
+    Vector<T> b = {1,2,3,-1,2,1};
+    /*
+        x in [1,1]
+        y in [-2, 2]
+        z in [-1, 3]
+    */
+
+    Vector<T> obj{1,0,0};
+    
+    SimplexMethodOptimizer optimizer;
+    
+    auto result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,1));
+
+    result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,1));
+
+    obj = {1,-1, 0};
+
+    result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,3));
+
+    result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,-1));
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_0_constraints, T, test_types)
+{
+    using namespace LinearAlgebra;
+    using namespace LinearAlgebra::Dense;
+
+    Matrix<T> A = {
+        {1,0,0},
+        {0,1,0},
+        {0,0,1},
+        {-1,0,0},
+        {0,-1,0},
+        {0,0,-1},
+    };
+
+    Vector<T> b = {10,7,16,0,0,0};
+    b /= T(10);
+    /*
+        x in [0,1]
+        y in [0,0.7]
+        z in [0,1.6]
+    */
+
+    Vector<T> obj{1,0,0};
+    
+    SimplexMethodOptimizer optimizer;
+    
+    auto result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,1));
+
+    result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,0));
+
+    obj = {1,-1,0};
+
+    result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,1));
+
+    result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    BOOST_CHECK(is_exactly<T>(result,T(-7)/10));
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_simplex_negative_constraints, T, test_types)
+{
+    using namespace LinearAlgebra;
+    using namespace LinearAlgebra::Dense;
+
+    Matrix<T> A = {
+        {1,0},
+        {0,1},
+        {-1,0},
+        {0,-1},
+    };
+
+    Vector<T> b = {995,1005,5,-5};
+    b /= T(100);
+    /*
+        x in [-0.05, 9.95]
+        y in [0.05, 10.05]
+    */
+
+    SimplexMethodOptimizer optimizer;
+
+    Vector<T> obj{1,-1};
+    
+    auto result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
+
+    BOOST_CHECK(approximate<T>(result,T(99)/10));
+
+    result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    BOOST_CHECK(approximate<T>(result,T(-101)/10));
+
+    obj = {1,0};
+    
+    result = optimizer(A,b,obj,OptimizationGoal::MAXIMIZE);
+    BOOST_CHECK(approximate<T>(result,T(995)/100));
+
+    result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    BOOST_CHECK(approximate<T>(result,T(-5)/100));
+}
+
+BOOST_AUTO_TEST_CASE(test_simplex_GPLK_bug)
+{
+    using namespace LinearAlgebra;
+    using namespace LinearAlgebra::Dense;
+
+    Matrix<double> A = {
+        {1,2.51581e-08},
+        {-2.51581e-08,1},
+        {-1,-2.51581e-08},
+        {2.51581e-08,-1}
+    };
+
+    Vector<double> b = {9.95,10.05,0.05,-0.05};
+    /*
+       1*x + 2.51581e-08*y <= 9.95
+       -2.51581e-08*x + 1*y <= 10.05
+       -1*x + -2.51581e-08*y <= 0.05
+       2.51581e-08*x + -1*y <= -0.05
+    */
+
+    SimplexMethodOptimizer optimizer;
+
+    Vector<double> obj{1,0};
+
+    auto result = optimizer(A,b,obj,OptimizationGoal::MINIMIZE);
+    auto objective_value = -0.05-2.52838e-07-9.04964e-13;
+
+    for (const auto& value: b-A*result.optimum()) {
+        BOOST_CHECK(value>=0);
+    }
+    BOOST_CHECK(approximate<double>(result,objective_value));
 }
