b916f1757f88d8328ac43e2e6b8b844f77215ce1
Alberto Casagrande <acasagrande@units.it>
2023-03-17 17:40:48 +0100

    Fixed a GCC compilation warning.

--- sapo/src/AutoGenerated.cpp
+++ sapo/src/AutoGenerated.cpp
@@ -268,6 +268,8 @@ DiscreteSystem<double> get_integrated_dynamics(const InputData &id,
     RungeKutta4Integrator rk4;
     return rk4(ode, id.getIntegrationStep());
   }
+  default:
+    throw std::runtime_error("Unsupported integrator");
   }
 }
 
